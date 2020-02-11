42a24a89b804496ff189b7938b90dc86764dd191
pull-vert <frederic.montariol@gmail.com>
2020-02-11 22:41:06 +0100

    update copyright header 2020

--- kotysa-spring-data-r2dbc/src/test/kotlin/com/pullvert/kotysa/r2dbc/sample/coroutinesSqlClientR2dbc.kt
+++ kotysa-spring-data-r2dbc/src/test/kotlin/com/pullvert/kotysa/r2dbc/sample/coroutinesSqlClientR2dbc.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 the original author or authors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2019-2020 the original author or authors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package com.pullvert.kotysa.r2dbc.sample
