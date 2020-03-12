e529f5983b895993a6265455033eb5b3fe1cf92b
pull-vert <frederic.montariol@gmail.com>
2020-03-12 17:12:23 +0100

    postgresql support (WIP22) : R2DbcAllTypesPostgresqlTest is done fixes #7

--- kotysa-core/src/testFixtures/kotlin/com/pullvert/kotysa/test/PostgresqlTestEntities.kt
+++ kotysa-core/src/testFixtures/kotlin/com/pullvert/kotysa/test/PostgresqlTestEntities.kt
@@ -24,42 +24,39 @@ val postgresqlTables =
                 column { it[PostgresqlUser::roleId].uuid().foreignKey<PostgresqlRole>("FK_users_roles") }
                 column { it[PostgresqlUser::alias].varchar() }
             }
-//            table<PostgresqlAllTypesNotNull> {
-//                name = "all_types"
-//                column { it[PostgresqlAllTypesNotNull::id].uuid().primaryKey() }
-//                column { it[PostgresqlAllTypesNotNull::string].varchar() }
-//                column { it[PostgresqlAllTypesNotNull::boolean].boolean() }
-//                column { it[PostgresqlAllTypesNotNull::localDate].date() }
-//                column { it[PostgresqlAllTypesNotNull::offsetDateTime].timestampWithTimeZone() }
-//                column { it[PostgresqlAllTypesNotNull::localTim].time9() }
-//                column { it[PostgresqlAllTypesNotNull::localDateTime1].dateTime() }
-//                column { it[PostgresqlAllTypesNotNull::localDateTime2].timestamp() }
-//                column { it[PostgresqlAllTypesNotNull::uuid].uuid() }
-//                column { it[PostgresqlAllTypesNotNull::int].int() }
-//            }
-//            table<PostgresqlAllTypesNullable> {
-//                name = "all_types_nullable"
-//                column { it[PostgresqlAllTypesNullable::id].uuid().primaryKey() }
-//                column { it[PostgresqlAllTypesNullable::string].varchar() }
-//                column { it[PostgresqlAllTypesNullable::localDate].date() }
-//                column { it[PostgresqlAllTypesNullable::offsetDateTime].timestampWithTimeZone() }
-//                column { it[PostgresqlAllTypesNullable::localTim].time9() }
-//                column { it[PostgresqlAllTypesNullable::localDateTime1].dateTime() }
-//                column { it[PostgresqlAllTypesNullable::localDateTime2].timestamp() }
-//                column { it[PostgresqlAllTypesNullable::uuid].uuid() }
-//                column { it[PostgresqlAllTypesNullable::int].int() }
-//            }
-//            table<PostgresqlAllTypesNullableDefaultValue> {
-//                column { it[PostgresqlAllTypesNullableDefaultValue::id].uuid().primaryKey() }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::string].varchar().defaultValue("default") }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::localDate].date().defaultValue(LocalDate.MAX) }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::offsetDateTime].timestampWithTimeZone().defaultValue(OffsetDateTime.of(2019, 11, 4, 0, 0, 0, 0, ZoneOffset.UTC)) }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::localTim].time9().defaultValue(LocalTime.MAX) }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::localDateTime1].dateTime().defaultValue(LocalDateTime.of(2018, 11, 4, 0, 0)) }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::localDateTime2].timestamp().defaultValue(LocalDateTime.of(2019, 11, 4, 0, 0)) }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::uuid].uuid().defaultValue(UUID.fromString(defaultUuid)) }
-//                column { it[PostgresqlAllTypesNullableDefaultValue::int].int().defaultValue(42) }
-//            }
+            table<PostgresqlAllTypesNotNull> {
+                name = "all_types"
+                column { it[PostgresqlAllTypesNotNull::id].uuid().primaryKey() }
+                column { it[PostgresqlAllTypesNotNull::string].varchar() }
+                column { it[PostgresqlAllTypesNotNull::boolean].boolean() }
+                column { it[PostgresqlAllTypesNotNull::localDate].date() }
+                column { it[PostgresqlAllTypesNotNull::offsetDateTime].timestampWithTimeZone() }
+                column { it[PostgresqlAllTypesNotNull::localTim].time() }
+                column { it[PostgresqlAllTypesNotNull::localDateTime].timestamp() }
+                column { it[PostgresqlAllTypesNotNull::uuid].uuid() }
+                column { it[PostgresqlAllTypesNotNull::int].integer() }
+            }
+            table<PostgresqlAllTypesNullable> {
+                name = "all_types_nullable"
+                column { it[PostgresqlAllTypesNullable::id].uuid().primaryKey() }
+                column { it[PostgresqlAllTypesNullable::string].varchar() }
+                column { it[PostgresqlAllTypesNullable::localDate].date() }
+                column { it[PostgresqlAllTypesNullable::offsetDateTime].timestampWithTimeZone() }
+                column { it[PostgresqlAllTypesNullable::localTim].time() }
+                column { it[PostgresqlAllTypesNullable::localDateTime].timestamp() }
+                column { it[PostgresqlAllTypesNullable::uuid].uuid() }
+                column { it[PostgresqlAllTypesNullable::int].integer() }
+            }
+            table<PostgresqlAllTypesNullableDefaultValue> {
+                column { it[PostgresqlAllTypesNullableDefaultValue::id].uuid().primaryKey() }
+                column { it[PostgresqlAllTypesNullableDefaultValue::string].varchar().defaultValue("default") }
+                column { it[PostgresqlAllTypesNullableDefaultValue::localDate].date().defaultValue(LocalDate.of(2019, 11, 4)) }
+                column { it[PostgresqlAllTypesNullableDefaultValue::offsetDateTime].timestampWithTimeZone().defaultValue(OffsetDateTime.of(2019, 11, 4, 0, 0, 0, 0, ZoneOffset.UTC)) }
+                column { it[PostgresqlAllTypesNullableDefaultValue::localTim].time().defaultValue(LocalTime.of(11, 25, 55)) }
+                column { it[PostgresqlAllTypesNullableDefaultValue::localDateTime].timestamp().defaultValue(LocalDateTime.of(2018, 11, 4, 0, 0)) }
+                column { it[PostgresqlAllTypesNullableDefaultValue::uuid].uuid().defaultValue(UUID.fromString(defaultUuid)) }
+                column { it[PostgresqlAllTypesNullableDefaultValue::int].integer().defaultValue(42) }
+            }
             table<PostgresqlUuid> {
                 column { it[PostgresqlUuid::id].uuid().primaryKey() }
                 column { it[PostgresqlUuid::roleIdNotNull].uuid().foreignKey<PostgresqlRole>() }
@@ -118,60 +115,91 @@ data class PostgresqlUser(
 val postgresqlJdoe = PostgresqlUser("John", "Doe", false, postgresqlUser.id)
 val postgresqlBboss = PostgresqlUser("Big", "Boss", true, postgresqlAdmin.id, "TheBoss")
 
-///**
-// * @author Fred Montariol
-// */
-//data class PostgresqlAllTypesNotNull(
-//        val id: UUID,
-//        val string: String,
-//        val boolean: Boolean,
-//        val localDate: LocalDate,
-//        val offsetDateTime: OffsetDateTime,
-//        val localTim: LocalTime,
-//        val localDateTime1: LocalDateTime,
-//        val localDateTime2: LocalDateTime,
-//        val uuid: UUID,
-//        val int: Int
-//)
-//
-//val postgresqlAllTypesNotNull = PostgresqlAllTypesNotNull(UUID.fromString("79e9eb45-2835-49c8-ad3b-c951b591bc7f"), "",
-//        true, LocalDate.now(), OffsetDateTime.now(), LocalTime.now(), LocalDateTime.now(), LocalDateTime.now(),
-//        UUID.randomUUID(), 1)
-//
-///**
-// * @author Fred Montariol
-// */
-//data class PostgresqlAllTypesNullable(
-//        val id: UUID,
-//        val string: String?,
-//        val localDate: LocalDate?,
-//        val offsetDateTime: OffsetDateTime?,
-//        val localTim: LocalTime?,
-//        val localDateTime1: LocalDateTime?,
-//        val localDateTime2: LocalDateTime?,
-//        val uuid: UUID?,
-//        val int: Int?
-//)
-//
-//val postgresqlAllTypesNullable = PostgresqlAllTypesNullable(UUID.fromString("67d4306e-d99d-4e54-8b1d-5b1e92691a4e"), null,
-//        null, null, null, null, null, null, null)
-//
-///**
-// * @author Fred Montariol
-// */
-//data class PostgresqlAllTypesNullableDefaultValue(
-//        val string: String? = null,
-//        val localDate: LocalDate? = null,
-//        val offsetDateTime: OffsetDateTime? = null,
-//        val localTim: LocalTime? = null,
-//        val localDateTime1: LocalDateTime? = null,
-//        val localDateTime2: LocalDateTime? = null,
-//        val uuid: UUID? = null,
-//        val int: Int? = null,
-//        val id: UUID = UUID.randomUUID()
-//)
-//
-//val postgresqlAllTypesNullableDefaultValue = PostgresqlAllTypesNullableDefaultValue()
+/**
+ * @author Fred Montariol
+ */
+data class PostgresqlAllTypesNotNull(
+        val id: UUID,
+        val string: String,
+        val boolean: Boolean,
+        val localDate: LocalDate,
+        val offsetDateTime: OffsetDateTime,
+        val localTim: LocalTime,
+        val localDateTime: LocalDateTime,
+        val uuid: UUID,
+        val int: Int
+)
+
+val postgresqlAllTypesNotNull = PostgresqlAllTypesNotNull(UUID.fromString("79e9eb45-2835-49c8-ad3b-c951b591bc7f"), "",
+        true, LocalDate.now(), OffsetDateTime.now(), LocalTime.now(), LocalDateTime.now(), UUID.randomUUID(), 1)
+
+/**
+ * @author Fred Montariol
+ */
+data class PostgresqlAllTypesNullable(
+        val id: UUID,
+        val string: String?,
+        val localDate: LocalDate?,
+        val offsetDateTime: OffsetDateTime?,
+        val localTim: LocalTime?,
+        val localDateTime: LocalDateTime?,
+        val uuid: UUID?,
+        val int: Int?
+)
+
+val postgresqlAllTypesNullable = PostgresqlAllTypesNullable(UUID.fromString("67d4306e-d99d-4e54-8b1d-5b1e92691a4e"), null,
+        null, null, null, null, null, null)
+
+/**
+ * @author Fred Montariol
+ */
+data class PostgresqlAllTypesNullableDefaultValue(
+        val string: String? = null,
+        val localDate: LocalDate? = null,
+        val offsetDateTime: OffsetDateTime? = null,
+        val localTim: LocalTime? = null,
+        val localDateTime: LocalDateTime? = null,
+        val uuid: UUID? = null,
+        val int: Int? = null,
+        val id: UUID = UUID.randomUUID()
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PostgresqlAllTypesNullableDefaultValue
+
+        if (string != other.string) return false
+        if (localDate != other.localDate) return false
+        if (offsetDateTime != null) {
+            if (!offsetDateTime.isEqual(other.offsetDateTime)) return false
+        } else if (other.offsetDateTime != null) {
+            return false
+        }
+        if (localTim != other.localTim) return false
+        if (localDateTime != other.localDateTime) return false
+        if (uuid != other.uuid) return false
+        if (int != other.int) return false
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = string?.hashCode() ?: 0
+        result = 31 * result + (localDate?.hashCode() ?: 0)
+        result = 31 * result + (offsetDateTime?.hashCode() ?: 0)
+        result = 31 * result + (localTim?.hashCode() ?: 0)
+        result = 31 * result + (localDateTime?.hashCode() ?: 0)
+        result = 31 * result + (uuid?.hashCode() ?: 0)
+        result = 31 * result + (int ?: 0)
+        result = 31 * result + id.hashCode()
+        return result
+    }
+}
+
+val postgresqlAllTypesNullableDefaultValue = PostgresqlAllTypesNullableDefaultValue()
 
 /**
  * @author Fred Montariol
